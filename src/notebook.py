4db693d13357b4f3be1e2616d64d55aaa50d2b57
josedaniel-06 <jmarulandacatano10@gmail.com>
2026-02-16 17:27:12 -0500

    Clases y atributos

--- src/notebook.py
+++ src/notebook.py
@@ -1,1 +1,18 @@
-# TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+import code
+
+
+class = Note:
+
+    HIGH = "HIGH"
+    MEDIUM = "MEDIUM"
+    LOW = "LOW"
+
+    def = __init__(self, code: str title: str text: str importance: str):
+    self.code = code
+    self.title = title
+    self. text = text
+    self.importance = importance
+    creation_date = datetime
+
+
+
