55ee0c0f0631685094435ca925ebeedbb6f97fba
josedaniel-06 <jmarulandacatano10@gmail.com>
2026-02-16 17:46:43 -0500

    Clases y atributos

--- src/notebook.py
+++ src/notebook.py
@@ -1,18 +1,15 @@
 import code
 
-
-class = Note:
+from datetime import datetime
+class Note:
 
     HIGH = "HIGH"
     MEDIUM = "MEDIUM"
     LOW = "LOW"
 
-    def = __init__(self, code: str title: str text: str importance: str):
-    self.code = code
-    self.title = title
-    self. text = text
-    self.importance = importance
-    creation_date = datetime
-
-
-
+    def  __init__(self, code: str, title: str, text: str, importance: str):
+         self.code = code
+         self.title = title
+         self.text = text
+         self.importance = importance
+         self.creation_date = datetime.now()
